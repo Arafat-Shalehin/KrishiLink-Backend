9d951afa8873041416b5f5989c9f5327e1e872fb
Arafat Shalehin <md.arafatshalehin@gmail.com>
2025-11-09 20:28:26 +0600

    Made apis for latest crop section.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
@@ -25,7 +26,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // const db = client.db("");
+    const db = client.db("Crops");
+
+    const cropsCollection = db.collection('allCrops');
+
+    app.get('/sixCrops', async (req, res) => {
+      const cursor = cropsCollection.find();
+      const result = await cursor.sort({pricePerUnit: 1}).limit(6).toArray();
+      res.send(result);
+    })
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
